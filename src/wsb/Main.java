471fbc7c454e1e4b82b58c9009d41c0c1ffc9d82
Joao Lima <joaoplima@ua.pt>
2021-02-27 09:31:26 +0100

    Task 6

--- src/wsb/Main.java
+++ src/wsb/Main.java
@@ -1,7 +1,14 @@
 package wsb;
 
-import wsb.creatures.*;
+import wsb.creatures.Animal;
+import wsb.creatures.Gender;
+import wsb.creatures.Human;
 import wsb.devices.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
@@ -33,6 +40,62 @@ public class Main {
         // Get the GDP from Argentina in PLN
         System.out.printf("%.0f",Country.ARGENTINA.getGdpInPLN());
         System.out.println("\nCountry Language: " + Country.PORTUGAL.getLanguage());
+
+        // HashMap
+        //Car(String producer, String model, Integer yearOfProduction, Double sizeOfAnEngine)
+        DieselCar car1 = new DieselCar("Kia", "Niro", 2009, 2.0);
+        DieselCar car2 = new DieselCar("Mazda", "CX-9", 2009, 2.0);
+        ElectricCar car3 = new ElectricCar("Ford", "model Y", 2009);
+        ElectricCar car4 = new ElectricCar("Ford", "model P", 2009);
+
+        Map<String, Double> garageUnsort = new HashMap<>();
+        garageUnsort.put(car1.model, 30000.0);
+        garageUnsort.put(car2.model, 10000.0);
+        garageUnsort.put(car3.model, 20000.0);
+        garageUnsort.put(car4.model, 140000.0);
+
+        System.out.println("Before sorting......");
+        System.out.println(garageUnsort);
+
+        // Get all the cars from garage
+        for (Map.Entry<String, Double> en : garageUnsort.entrySet()) {
+            System.out.println("Key = " + en.getKey() +
+                    ", Value = " + en.getValue());
+        }
+
+        for(String car: garageUnsort.keySet()){
+            System.out.println("Model: " + car + " | Producer: " + garageUnsort.get(car));
+        }
+
+        // Create new map with the areas
+        Map<Country, Double> countryAreaMap = new HashMap<>();
+        garageUnsort.put(Country.ARGENTINA.getCountryCode(), Country.ARGENTINA.getArea());
+        garageUnsort.put(Country.POLAND.getCountryCode(), Country.POLAND.getArea());
+        garageUnsort.put(Country.PORTUGAL.getCountryCode(), Country.PORTUGAL.getArea());
+        garageUnsort.put(Country.SPAIN.getCountryCode(), Country.SPAIN.getArea());
+
+        // Task 6
+        HashMap<String, List<Device>> producers = new HashMap<>();
+        List<Device> producedBySiemens = new ArrayList<Device>() {{
+            add(new Phone("Siemens", "model X", 5.1, OperatingSystem.ANDROID));
+        }};
+        producers.put("Siemens", producedBySiemens);
+
+        List<Device> producedByHuawei = new ArrayList<Device>() {{
+            add(new Phone("Huawei", "model Y", 5.2, OperatingSystem.ANDROID));
+        }};
+        producers.put("Huawei", producedBySiemens);
+
+        List<Device> producedByFord = new ArrayList<Device>() {{
+            add(car3);
+            add(car2);
+        }};
+        producers.put("Ford", producedByFord);
+
+        // Find all devices produced by Ford and Siemens
+        producers.get("Siemens").forEach(System.out::println);
+        producers.get("Ford").forEach(System.out::println);
+
 
 
 
