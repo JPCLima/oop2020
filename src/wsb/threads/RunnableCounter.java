4c917a38971fc985ce5b957ae79df24c97e84c25
Joao Lima <joaoplima@ua.pt>
2021-03-20 12:34:06 +0100

    Task 9 a

---
+++ src/wsb/threads/RunnableCounter.java
@@ -0,0 +1,18 @@
+package wsb.threads;
+
+public class RunnableCounter implements Runnable {
+
+    static final public Integer START_FROM = 4;
+
+    @Override
+    public void run() {
+        for (int i = START_FROM; i >= 0 ; i--) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
